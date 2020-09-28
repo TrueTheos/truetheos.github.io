2118a8b21ecf9a95b63e38cbb195075b4d69f510
Theos <jeremi2811@wp.pl>
2020-09-28 20:37:23 +0200

    Publish

---
+++ js/projects.js
@@ -0,0 +1,20 @@
+//I know that this is a dirty method, but tbh it's just a static webpage
+const projects = [
+    {
+      projectName: 'Tower of Blood',
+      lang: 'C#',
+      langColor: '#9D6ED8',
+      description: 'ASCII & Pixelart roguelike.',
+      link: 'https://github.com/Loiuy123/Bloody-Tower',
+      technologies: [
+        {
+          name: 'C#',
+          color: '#189F20'
+        },
+        {
+          name: 'Unity',
+          color: '#317CFF'
+        }
+      ]
+    }
+  ]
